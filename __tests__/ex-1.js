d540e51d235aa1f91a9d3c1534febeb1369f9f1b
Bhargav <ladbhargav@yahoo.co.in>
2022-03-22 22:03:54 -0400

    Fix failing test.
    Failing test was due to double decleration of names variable

--- __tests__/ex-1.js
+++ __tests__/ex-1.js
@@ -4,7 +4,6 @@ const PrintAll = require("../Exercises/ex-1");
 describe("Test for Ex-1", () => {
   it("should print all names in array", () => {
     const names = ["Ben", "Jafar", "Matt", "Priya", "Brian"];
-    const names = ["Ben", "Jafar", "Matt", "Priya", "Brian"];
     const answers = PrintAll();
     names.map((name, i) => expect(name).to.equal(answers[i]));
   });
